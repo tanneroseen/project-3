c4b2b03b4bd5a63669ff5f08abc0cd22dee62d87
gavinscott1 <gavin.scott1@ucalgary.ca>
2022-12-03 16:33:52 -0700

    Imported wind graph, no explanation yet

--- test.py
+++ test.py
@@ -28,6 +28,34 @@ max_grouped_by_week = jasper_data.groupby(pd.Grouper(key = 'Date (Local Standard
 date_range = min_grouped_by_week.keys() #grabs each date (start of each week grouped by) that is used by all graphs as common x-values
 differce_in_max_and_min = max_grouped_by_week - min_grouped_by_week
 
+#All for Windspeed graph below
+monthly_average_windspeed = jasper_data.groupby(pd.Grouper(key='Date (Local Standard Time)',freq='MS'))['Wind Speed 10 m Avg. (km/h)'].mean().reset_index()  #Groups wind speed data by the respective month and takes the average of it
+monthly_average_windspeed_direction = jasper_data.groupby(pd.Grouper(key='Date (Local Standard Time)',freq='MS'))['Wind Dir. 10 m Avg. '].mean().reset_index() #groups wind direction average in each month and takes the average of all days avg direction
+
+r = monthly_average_windspeed['Wind Speed 10 m Avg. (km/h)'] #Makes the magnitude of the polar plot the monthly avg windspeed
+theta = monthly_average_windspeed_direction['Wind Dir. 10 m Avg. '] #makes the angle (or position on the polar plot the avg direction for each month)
+
+monthly_average_windspeed_direction['Compass Direction'] = None #Creates an extra column to populate in the following loop
+
+for count, direction in enumerate(monthly_average_windspeed_direction['Wind Dir. 10 m Avg. ']):  #Between degree values, the values are assigned a direction based on a compass and they are shown in the figure when you hover your mouse over plotted values.
+    if 112.5 < direction < 157.5:
+        monthly_average_windspeed_direction.iloc[count, 2] = 'S-E'
+    elif 157.5 < direction < 202.5:
+        monthly_average_windspeed_direction.iloc[count, 2] = 'South'
+    if 202.5 < direction < 247.5:
+        monthly_average_windspeed_direction.iloc[count, 2] = 'S-W'
+    elif 247.5 < direction < 292.5:
+        monthly_average_windspeed_direction.iloc[count, 2] = 'West'
+    if 292.5 < direction < 337.5:
+        monthly_average_windspeed_direction.iloc[count, 2] = 'N-W'
+    elif (337.5 < direction < 360) or (0 < direction < 22.5):
+        monthly_average_windspeed_direction.iloc[count, 2] = 'North'
+    elif (22.5 < direction < 67.5):
+        monthly_average_windspeed_direction.iloc[count, 2] = 'N-E'
+
+monthly_average_windspeed_direction['Date as String'] = monthly_average_windspeed_direction['Date (Local Standard Time)'].dt.strftime('%b %Y') #Converting the dates in the dataframe into strings so that they can be displayed in the graph easily.
+#All for windspeed graph above
+
 fancy_page_stuff = """
 <style>
 [data-testid="stAppViewContainer"] > .main{
@@ -158,10 +186,38 @@ if 'Temperature' in option:
         )
 
 if 'Wind' in option:
-    fig2 = go.Figure()
-    fig2.add_trace(go.Scatter(
-        x=pd.date_range("2019-10-03", "2022-11-03", freq='M'),
-        y=min_grouped_by_week),
+    fig2 = go.Figure() #Initializes the figure
+
+    fig2.add_trace(go.Scatterpolar( #Polar scatter plot is created with all of the listed values
+        r = r,
+        theta = theta,
+        mode = 'markers',
+        marker = dict(
+            size=2.5*r,             #Adds variable marker size depending on the magnitude of the wind speed
+            color=r,                #Colour is also dynamic with the magnitude of the wind speed
+            colorbar=dict(          #Colour bar (displayed on the right of the graph) is generated showing the colour mapped with the magnitude.
+                title="Wind Speed Range (km/h)"
+            ),
+            colorscale="plasma"
+    )))
+
+
+    fig2.update_traces(
+        text=monthly_average_windspeed_direction.iloc[:,[3,2]], #Hover text is defined by the date(type: str) and the compass directio(type: str)
+        hovertemplate = 'Date: %{text[0]} <extra></extra>' + 
+        '<br>Wind speed: %{r:.2f} km/h' +                       #Custom hover text which is displayed when the cursor hovers over a value
+        '<br>Direction: %{theta:.2f}°, %{text[1]}',
+    )
+
+    fig2.update_layout(
+        title='Monthly Average Windspeed and Direction Recorded in Jasper National Park',
+        font_size=16,
+        legend_font_size=16,
+        polar_radialaxis_ticksuffix='',
+        polar_angularaxis_rotation=90,
+        polar_angularaxis_direction = 'clockwise',
+        plot_bgcolor = 'rgba(0,0,0,0.2)',
+        paper_bgcolor = 'rgba(0,0,0,0)'
     )
 
     st.plotly_chart(fig2)
