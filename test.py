9d54f6756f185054ede45ac8d602bda166393433
tanneroseen <tanneroseen@gmail.com>
2022-12-05 12:26:06 -0700

    Update

--- test.py
+++ test.py
@@ -228,7 +228,10 @@ if 'Wind' in option:
     fig2.update_layout(
         title=dict(
             'Monthly Average Windspeed and Direction Recorded in Jasper National Park',
-            size=14,
+            font=dict(
+                size=14,
+            )
+            
         ),
         polar_radialaxis_ticksuffix='',
         polar_angularaxis_rotation=90,
